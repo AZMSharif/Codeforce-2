a7e7fccc55b4cdc84784f0ef9ff86dd5953867d4
AZMSharif <110534962+AZMSharif@users.noreply.github.com>
2022-10-05 19:59:29 +0600

    Create W.cpp

---
+++ W.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int r,a,c;
+    cin>>a;
+
+    for(r=1; r<=a; r++)
+    {
+        for(c=1; c<=a-r; c++)
+        {
+            cout<<" ";
+        }
+        for(c=1; c<=2*r-1; c++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(r=a; r>=1; r--)
+    {
+        for(c=1; c<=a-r; c++)
+        {
+            cout<<" ";
+        }
+        for(c=1; c<=2*r-1; c++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
