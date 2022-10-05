14aedde9c9ce73270ad3574d077bd4e2f4326e5d
AZMSharif <110534962+AZMSharif@users.noreply.github.com>
2022-10-05 19:23:21 +0600

    Create V.cpp

---
+++ V.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int i,j,n,m=0;
+    cin>>n;
+    for(i=1; i<=n*4; i++)
+    {
+     if(i%4==0){
+        cout<<"PUM"<<endl;
+    }
+    else
+    cout<<i<<" ";
+}
+return 0;
+}
